4e5078e3aba774fad3d9da12724b37cc3216bfce
PradeepGorle4 <pradeepgorle4@gmail.com>
2025-06-14 07:54:52 +0530

    terraform-aws-eks-infra

---
+++ 10-sg/provider.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>5.95.0"  # Adjust as needed for compatibility
+      
+    }
+  }
+
+  backend "s3" {
+    bucket         = "expense-infra-eks-dev-state-locking"
+    key            = "expense-dev-eks-sg" # Unique key should be used with in the bucket, this will dump in our bucket only if others have same key and access.
+    region         = "us-east-1"
+    use_lockfile = true
+  }
+}
+
+
+provider "aws" {
+  # Configuration options
+  region = "us-east-1"
+}
